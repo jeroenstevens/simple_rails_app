242296d34f389f443ce61340e023296d2472094c
Jefferson Girao <jgirao@thoughtworks.com>
2013-12-08 13:25:53 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple Rails App
+================
+
+This is a simple rails application used to demonstrate a provisioning and deployment processed based on Ansible
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleRailsApp::Application.config.secret_key_base = 'be76d276fd342c255e2f7f7d4d40be4c64138bc990a026f77aebc33dce99a4ffd1d952f466943a2a84aebec47062930b2cb2627df02e5747c741903b827bcbbf'
